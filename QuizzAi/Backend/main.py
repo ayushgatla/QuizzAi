5884e74671634eb3d810ec18b596a69365f30d64
ayushgatla <ayushgatla@gmail.com>
2025-12-06 20:31:19 +0530

    agents_configured

--- QuizzAi/Backend/main.py
+++ QuizzAi/Backend/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from typing import Dict
 from crewai import Crew, Task, Agent
 from config import Config
-from utils.crew import run_agent,add_to_memory
+from utils.crew import agent_maneger
 from utils.pdf_parser import extract_text
 from utils.sessions import session_manager
 import os
@@ -46,7 +46,7 @@ async def upload(chat_id:str , file: UploadFile = File(...)):
     session["pdf_text"]=text
     session["processed"]=True
 
-    add_to_memory(session["crew_session_id"],text)
+    agent_maneger.add_to_memory(session["crew_session_id"],text)
     os.remove(file_path)
     return {"message": "PDF processed & memory updated"}
 
@@ -63,7 +63,7 @@ def generate(chat_id:str ,type:str,count:int):
         prompt = f"Generate {count} short questions from the stored pdf content and given them in a json file with question,answer,correct answer,explanation why it is correct"
     elif type=="long":
         prompt = f"Generate {count} long questions from the stored pdf content and given them in a json file with question,answer,correct answer,explanation why it is correct"
-    result = run_agent(crew_id,prompt)    
+    result = agent_maneger.run_agent(prompt,crew_id)    
     return {"result":result}
 
 @app.get("/session/{chat_id}")
@@ -74,7 +74,7 @@ def get_session_info(chat_id:str):
     else:
         return session
     
-    
+@app.post("/session")    
 
 
 
